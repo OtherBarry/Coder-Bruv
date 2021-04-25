ca64cf99e67235f1cd48f100880c02cb0e8bdd9d
OtherBarry <charlie.wilson4@gmail.com>
2021-04-25 21:57:40 +1000

    Attempt 1

--- python3/app/bot/agent.py
+++ python3/app/bot/agent.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import networkx as nx
+import math
 
 from ..utilities import PriorityQueue
 from ..server_connection import ServerConnection
@@ -23,9 +24,11 @@ def _get_direction_from_coords(start, end):
     else:
         raise ValueError
 
+def _manhattan_distance(start, end):
+    return abs(end[0] - start[0]) + abs(end[1] - start[1])
 
 def _manhattan_to_centre(start_node):
-    return abs(4 - start_node[0]) + abs(4 - start_node[1])
+    return _manhattan_distance(start_node, (4, 4))
 
 
 class Agent:
@@ -58,7 +61,7 @@ class Agent:
                 best_nodes.push((node_value, self.distances[node], node))
 
         while not best_nodes.is_empty():
-            current_best = best_nodes.pop()[2]  # For the lowest weight node
+            current_best = best_nodes.pop()[2]
             current_best_path = self.paths[
                 current_best
             ]
@@ -66,11 +69,7 @@ class Agent:
             if (worst_node < travel_threshold):
                 return current_best_path
 
-        # print("No paths found", end=" | ")
-
-
-
-    def find_danger_nodes(self):
+    def _find_danger_nodes(self):
         to_visit = list(nx.articulation_points(self.map.graph))
         danger_nodes = set()
         while to_visit:
@@ -86,7 +85,7 @@ class Agent:
                 danger_nodes.add(node)
         return danger_nodes
 
-    def find_danger_entrance(self, coord):
+    def _find_danger_entrance(self, coord):
         if coord not in self.danger_nodes:
             return None
         to_visit = [coord]
@@ -101,20 +100,49 @@ class Agent:
             to_visit.extend(neighbours)
             visited.append(current)
 
-    def is_enemy_trapped(self, cutoff = 101):
+    def _is_enemy_trapped(self, cutoff=101):
         if self.us.ammo > 0 and self.them.coords in self.danger_nodes:
-            entrance = self.find_danger_entrance(self.them.coords)
+            entrance = self._find_danger_entrance(self.them.coords)
             path = self.paths.get(self.them.coords)
-            if path is None:
+            if path is None or entrance not in path:
                 return None, False
-            path = self.paths[self.them.coords]
-            if entrance not in path:
-                return None, False
-            guaranteed_roast = path.index(entrance) >= len(path) // 2
             worst = max(self._get_node_weight(x) for x in path)
             if worst < cutoff:
+                guaranteed_roast = path.index(entrance) >= len(path) // 2
                 return self.paths[entrance], guaranteed_roast
         return None, False
+
+    def _generate_neighbouring_tiles(self, coords):
+        for i, m in [(1, 1), (0, 1), (1, -1), (0, -1)]:
+            new = list(coords)
+            new[i] += m
+            new = tuple(new)
+            if new in self.map.graph:
+                yield new
+
+    def _dismount_bomb(self):
+        neighbours = []
+        distance_to_enemy = _manhattan_distance(self.us.coords, self.them.coords)
+        for neighbour in self._generate_neighbouring_tiles(self.us.coords):
+            if _manhattan_distance(neighbour, self.them.coords) >= distance_to_enemy:
+                neighbours.append(neighbour)
+        best_weight = math.inf
+        best_nodes = set()
+        for neighbour in neighbours:
+            distances, paths = nx.single_source_dijkstra(self.map.graph, neighbour, weight=self._get_edge_weight)
+            for node in distances:
+                mean_weight = distances[node] / len(paths[node])
+                if mean_weight < best_weight:
+                    best_weight = mean_weight
+                    best_nodes = {neighbour, }
+                elif mean_weight == best_weight:
+                    best_nodes.add(neighbour)
+        if len(best_nodes) > 1:
+            return max(best_nodes, key=lambda x: len(nx.node_connected_component(self.map.graph, x)))
+        for node in best_nodes:
+            return node
+        for node in self._generate_neighbouring_tiles(self.us.coords):
+            return node
 
     async def _on_game_tick(self, tick_number, game_state):
         if game_state is not self.state:
@@ -130,18 +158,11 @@ class Agent:
         # TODO: Handle existence of other player
         # TODO: Break out of cages
 
-        self.danger_nodes = self.find_danger_nodes()
+        self.danger_nodes = self._find_danger_nodes()
 
         if self.us.coords not in self.map.graph:
-            visitable = []
-            for i, m in [(1, 1), (0, 1), (1, -1), (0, -1)]:
-                new = list(self.us.coords)
-                new[i] += m
-                new = tuple(new)
-                if new in self.map.graph:
-                    visitable.append(new)
-            best = min(visitable, key = lambda x: self._get_node_weight(x))
-            move = _get_direction_from_coords(self.us.coords, best)
+            target = self._dismount_bomb()
+            move = _get_direction_from_coords(self.us.coords, target)
             await self._server.send_move(move)
             return
 
@@ -150,7 +171,7 @@ class Agent:
         )  # Get best paths to all nodes
         # print(self.danger_nodes, end=" | ")
 
-        path, guaranteed = self.is_enemy_trapped()
+        path, guaranteed = self._is_enemy_trapped()
         print(path, guaranteed, end=" | ")
         if path is not None and len(path) <= 1:
             await self._server.send_bomb()
